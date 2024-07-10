8ea69c5cb3ea62587a58858e15a8281d7cf6354d
ITX-FARYAL <122507892+ITX-FARYAL@users.noreply.github.com>
2024-07-10 08:06:32 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '923140529832' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=fBUHBQaS#BXM-EtlIf8JVLgGXJEgBElgy9VWTSW6KU4ohiu5XRsM' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "923259712710,923xxxxxxxx" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
